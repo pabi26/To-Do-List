0206f9cd30e82317d1f2211bf78c31d81380d5e4
prabdeep thind <prabdeep_thind@hotmail.com>
2023-03-14 15:57:20 -0400

    local storage issues fixed

--- index.js
+++ index.js
@@ -1,37 +1,70 @@
+let taskArray = [];
 const inputTask = document.getElementById('input-task');
 const taskList = document.getElementById('task-list');
-const addItemBtn = document.getElementById('add-item-button')
+const addTaskBtn = document.getElementById('add-item-button');
 
-addItemBtn?.addEventListener("click", function(event){
+addTaskBtn?.addEventListener("click", function(event){
     console.log('clicked');
     if (!inputTask || !inputTask.value) {
-        event.preventDefault();
-        document.getElementById('warning').textContent = "Enter Task";
+      event.preventDefault();
+      document.getElementById('warning').textContent = "Enter Task";
     } else {
-        addTask()
+      addTask(inputTask.value, false);
     }
-});
-
-
-
-
-function addTask() {
+  });
+  
+function addTask(taskText, isLoadedFromStorage) {
     // Create a new task item
     const newTask = document.createElement('li');
-    newTask.innerText = "-" + " " + inputTask.value;
-
+    newTask.innerText = "- " + taskText;
+  
+    // Check if the task already exists in the task list
+    if (taskList.querySelector(`li[data-task="${taskText}"]`)) {
+      return;
+    }
+  
     // Add a delete button to the task item
     const deleteButton = document.createElement('button');
     deleteButton.innerText = 'Delete';
     deleteButton.onclick = function() {
-    newTask.remove();
+      newTask.remove();
+      localStorage.removeItem(taskText);
     };
     newTask.appendChild(deleteButton);
-
+  
     // Add the new task item to the task list
     taskList.appendChild(newTask);
-
+    if (!isLoadedFromStorage) {
+      taskArray.push(taskText);
+    }
+  
     // Clear the input field
     inputTask.value = '';
     document.getElementById('warning').textContent = "";
+  
+    // Store the task item in local storage
+    localStorage.setItem('taskArray', JSON.stringify(taskArray));
+    localStorage.setItem(taskText, true);
+  }
+  
+
+// Load existing task items from local storage
+if (localStorage.getItem('taskArray')) {
+    taskArray = JSON.parse(localStorage.getItem('taskArray'));
+    taskArray.forEach(function(taskText) {
+        const taskListItem = document.createElement('li');
+        taskListItem.innerText = "- " + taskText;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.onclick = function() {
+            taskListItem.remove();
+            localStorage.removeItem(taskText);
+            taskArray.splice(taskArray.indexOf(taskText), 1);
+            localStorage.setItem('taskArray', JSON.stringify(taskArray));
+        };
+        taskListItem.appendChild(deleteButton);
+
+        taskList.appendChild(taskListItem);
+    });
 }
